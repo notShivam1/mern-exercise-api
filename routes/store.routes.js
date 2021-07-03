ab5455ea7ec911feaab55b5fa08e0d4ead845bf0
MadSadLad <shivam.119966@yahoo.com>
2021-07-03 21:50:47 +0530

    little

--- routes/store.routes.js
+++ routes/store.routes.js
@@ -3,14 +3,14 @@ const {
   createStore,
   getStores,
   getStore,
-  updateStore,
-  deleteStore,
+  // updateStore,
+  // deleteStore,
 } = require("../controllers/store.controller");
 
 router.post("/", createStore);
-router.get("/", getStores);
-router.get("/:storeid", getStore);
-router.put("/:storeid", updateStore);
-router.delete("/:storeid", deleteStore);
+router.get("/", getStore);
+router.get("/:googleId", getStores);
+// router.put("/:storeid", updateStore);
+// router.delete("/:storeid", deleteStore);
 
 module.exports = router;
